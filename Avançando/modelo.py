6bac346726c06e4217c0e1e4ad2a310a6ded70ab
Kamila Jesus <kamilajesusdev@gmail.com>
2022-10-07 00:34:33 -0300

    Add files via upload

--- Avançando/modelo.py
+++ Avançando/modelo.py
@@ -19,23 +19,52 @@ class Programa:
   def nome(self, novo_nome):
     self._nome = novo_nome.title()
     
+  def __str__(self):
+    return f'{self._nome} - {self.ano} - {self._likes} Likes'
+    
 
 class Filme(Programa):
   def __init__(self, nome, ano, duracao): 
     super().__init__(nome, ano)
     self.duracao = duracao
-
+  
+  def __str__(self):
+   return f'{self._nome} - {self.ano} - {self.duracao} min - {self._likes} Likes'
+    
   
 class Serie(Programa):
   def __init__(self, nome, ano, temporadas): 
     super().__init__(nome, ano)
     self.temporadas = temporadas  
+  
+  def __str__(self):
+    return f'{self._nome} - {self.ano} - {self.temporadas} temporadas - {self._likes} Likes'
+
+class Playlist:
+  def __init__(self, nome, programas):
+    self.nome = nome
+    self._programas = programas
     
-    
-    
+  @property
+  def listagem(self):
+    return self._programas
+  
+  @property
+  def tamanho(self):
+    return len(self._programas)
+  
 vingadores = Filme('vingadores - guerra infinita', 2018, 160)
-print(f'{vingadores.nome} -  {vingadores.duracao} - {vingadores.likes}')
-
 atlanta = Serie('atlanta', 2018, 2)
-print(f' {atlanta.nome} - {atlanta.temporadas} - {atlanta.likes}')
+tmep = Filme('todo mundo em pânico', 1999, 100)
+demolidor = Serie('Demolidor', 2016, 2)
 
+
+filmes_e_series = [vingadores, atlanta, demolidor, tmep]
+playlist_fim_de_semana = Playlist('fim de semana', filmes_e_series)
+
+print(f'Tamanho do playlist: {len(playlist_fim_de_semana.listagem)}')
+
+for programa in playlist_fim_de_semana.listagem:
+  print(programa)
+  
+print(f'ta ou não tá? {demolidor in playlist_fim_de_semana.listagem}')
