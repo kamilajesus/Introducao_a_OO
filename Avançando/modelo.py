7095e907c3fc5c4e04f3bb4315370d31b222b39f
Kamila Jesus <kamilajesusdev@gmail.com>
2022-10-07 23:44:59 -0300

    Add files via upload

--- Avançando/modelo.py
+++ Avançando/modelo.py
@@ -45,12 +45,14 @@ class Playlist:
     self.nome = nome
     self._programas = programas
     
+  def __getitem__(self, item):
+    return self._programas[item]
+    
   @property
   def listagem(self):
     return self._programas
   
-  @property
-  def tamanho(self):
+  def __len__(self):
     return len(self._programas)
   
 vingadores = Filme('vingadores - guerra infinita', 2018, 160)
@@ -62,9 +64,10 @@ demolidor = Serie('Demolidor', 2016, 2)
 filmes_e_series = [vingadores, atlanta, demolidor, tmep]
 playlist_fim_de_semana = Playlist('fim de semana', filmes_e_series)
 
-print(f'Tamanho do playlist: {len(playlist_fim_de_semana.listagem)}')
+print(f'Tamanho do playlist: {len(playlist_fim_de_semana)}')
 
-for programa in playlist_fim_de_semana.listagem:
+len(playlist_fim_de_semana)
+
+for programa in playlist_fim_de_semana:
   print(programa)
   
-print(f'ta ou não tá? {demolidor in playlist_fim_de_semana.listagem}')
